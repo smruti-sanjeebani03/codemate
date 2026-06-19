b81575b9ba66449cadfc752fa7cc5fbca272a065
Smruti Sanjeebani <smrutisanjeebani0310@gmail.com>
2026-06-19 23:06:11 +0530

    Initial commit

---
+++ src/main/java/com/smruti/codemate/CodemateApplication.java
@@ -0,0 +1,13 @@
+package com.smruti.codemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smruti/codemate/CodemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smruti.codemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
