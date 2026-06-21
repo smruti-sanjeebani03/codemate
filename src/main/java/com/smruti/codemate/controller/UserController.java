a78d404366ad05e7118813f37df662774329fb7e
Smruti Sanjeebani <smrutisanjeebani0310@gmail.com>
2026-06-21 22:03:35 +0530

    User Controller

---
+++ src/main/java/com/smruti/codemate/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.smruti.codemate.controller;
+
+import com.smruti.codemate.model.User;
+import com.smruti.codemate.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
